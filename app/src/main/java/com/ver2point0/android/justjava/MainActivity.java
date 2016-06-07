d730fb235dc0531cc47067fc9d4f393bbb40d193
John McIntosh II <jmci10@hotmail.com>
2016-06-06 23:26:10 -0400

    add coffee variable

--- app/src/main/java/com/ver2point0/android/justjava/MainActivity.java
+++ app/src/main/java/com/ver2point0/android/justjava/MainActivity.java
@@ -22,8 +22,9 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2 * 5);
+        int coffeeQuantity = 10;
+        display(coffeeQuantity);
+        displayPrice(coffeeQuantity * 3);
     }
 
     /**
